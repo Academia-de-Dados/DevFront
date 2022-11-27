c2cc04c9fab8e8e17a645e6ddd1949b4048f8022
cairon-henrique-60 <caironhenrique60@gmail.com>
2022-11-27 19:14:37 -0300

    Refactoring. Refatorando a telad e login

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,28 +1,34 @@
 import React from "react";
 import { FormLogin } from "../../styles/style";
 import { Button } from "../../tools/Button";
+import  imageLogo from "../../assets/logo.png"
 
 const Login: React.FC = () => {
   return (
     <>  
         <FormLogin>
             <div className="divMaster">
-                <h1>Academia de Dados</h1>
-                <form action="">
-                    <div>
-                        <label htmlFor="">Usuário</label>
-                        <input type="text" placeholder="Digite seu e-mail" id="user"/>
-                    </div>
-                    <div>
-                        <label htmlFor="">Digite sua senha</label>
-                        <input type="password" placeholder="Digite sua senha" id="user"/>
-                    </div>
-                    <div>  
-                        <Button>Entrar</Button>
-                    </div>
-                </form>
-                <a href="">Sou novo aqui!</a>
-                <a href="">Esqueci minha senha!</a>
+                <div className="logo">
+                    <img src={imageLogo} alt="logo do site" />
+                </div>
+                <div className="formLogin">
+                    <h1>Academia de Dados</h1>
+                    <form action="">
+                        <div>
+                            <label htmlFor="">Usuário</label>
+                            <input type="text" placeholder="Digite seu e-mail" id="user"/>
+                        </div>
+                        <div>
+                            <label htmlFor="">Digite sua senha</label>
+                            <input type="password" placeholder="Digite sua senha" id="user"/>
+                        </div>
+                        <div>  
+                            <Button>Entrar</Button>
+                        </div>
+                    </form>
+                    <a href="">Sou novo aqui!</a>
+                    <a href="">Esqueci minha senha!</a>
+                </div>
             </div>
         </FormLogin>
     </>
